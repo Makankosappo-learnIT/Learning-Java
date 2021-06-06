d33c5d7d69dc1e19637e8276d800ab8bcc59b975
Makankosappo-learnIT <84960911+Makankosappo-learnIT@users.noreply.github.com>
2021-06-06 11:03:28 +0800

    Add files via upload

---
+++ Days/Day1/pack/Hand.java
@@ -0,0 +1,15 @@
+package pack;
+
+public class Hand{
+    protected int amount;
+    public Hand(){
+        System.out.println("Hand Bro is a professional IT developer");
+    }
+    public void setAmount(int input){
+        this.amount = input;
+    }
+    public int getAmount(){
+        return this.amount;
+    }
+}
+
